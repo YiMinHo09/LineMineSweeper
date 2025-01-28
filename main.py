e18e42e1c5c83ec6a5343ba43b2d96e36b0ba945
YIMinHo09 <minho090707@naver.com>
2025-01-28 20:21:01 +0900

    prototype ready

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import random
-from turtle import TurtleGraphicsError
 
 private_number = ["공지", "지뢰", "활로"] # 프런트 표기용 리스트 (고유 번호를 넣어 텍스트로 변환)
 contain = [14, 9, 2] # 공지, 지뢰, 활로 갯수
 ground = []
 # 맵 생성
-while len(ground) < sum(contain):
-    i = random.randrange(0, 3)
-    print("i:", i)
-    if contain[i]:
-        print("remain:", contain[i])
-        ground.append({"room": i, "opened": 0}) # 방 종류, 방이 밝혀졌는지
-        contain[i] -= 1
-    print("len:", len(ground))
+for i in range(sum(contain)):
+    # 방 종류 무작위 선택 (제비뽑기 해서 순서대로 배치하는 방식)
+    print("list:", contain)
+    room = random.randrange(1, sum(contain)+1)
+    print("choice:", room)
+    if room <= contain[0]:
+        room_num = 0
+    elif room <= contain[0] + contain[1]:
+        room_num = 1
+    else:
+        room_num = 2
+    print("type:", room_num)
+    ##
+    #
+    if contain[room_num]:
+        print("remain:", contain[room_num])
+        ground.append({"room": room_num, "available": 1}) # 방 종류, 방이 밝혀졌는지
+        contain[room_num] -= 1
+    else: # 테스트 코드인데 혹시 몰라서 남김
+        print("!예외 발생!")
+    print("len:", len(ground), "\n")
 ##
 print(ground)
 
 # 인게임에서 사용하는 데이터
-winner = 0 # 탈출자 수
+winner = [] # 탈출자 목록
 survivor = [] # 게임 중인 인원 목록
 # 순서 결정
 for i in range(9):
@@ -27,20 +39,34 @@ survivor.insert(random.randrange(0, 10), "player")
 print(survivor)
 # 플레이어 행동
 def entry(target, player): # target = 방 번호, player = 선수 이름
-    if ground[target]["opened"]: # 닫힌 방만 들어갈 수 있음
+    if ground[target]["available"]: # 닫힌 방만 들어갈 수 있음
+        print("%s번 방은 %s입니다." % (target, private_number[ground[target]["room"]]))
         global winner
-        if ground[target]["room"] == 1:
-            survivor.remove(player)
-        elif ground[target]["room"] == 2:
-            survivor.remove(player)
-            winner += 1
-        ground[target]["opened"] = 1
+        if ground[target]["room"] == 1: # 탈락처리
+            print("%s, 탈락입니다." % player)
+            survivor.remove(player) ##
+        elif ground[target]["room"] == 2: # 통과처리
+            print("%s, 통과입니다." % player)
+            winner.append(player)
+            survivor.remove(player) ##
+        # 힌트 공개
+        ground[target]["available"] = 0
+        print(target, ground[target-1], ground[target+1])
+        print("%s번방의 힌트 번호는 " % target, end="")
+        if target == len(ground)-1:
+            print(ground[target-1]["room"] + ground[0]["room"], "입니다.")
+        else:
+            print(ground[target-1]["room"] + ground[target+1]["room"], "입니다.") ##
 ##
 # 게임 루프
-while True:
-    for i in survivor: # 순서 진행
+while len(survivor) + len(winner) > 2: # TODO : 이 조건이 entry 안에 들어가야 함
+    back_up = list(survivor)
+    for i in back_up: # 순서 진행
         print("==="+i+"===")
         if i == "player": # 플레이어 차례
-            input()
+            entry(int(input("들어갈 방 번호")), i)
         if "bot" in i: # 게임 에이아이
             pass
+    input("break point")
+print("게임이 종료되었습니다.")
+##
